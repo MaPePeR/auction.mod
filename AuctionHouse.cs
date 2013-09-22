bafeeba653342b63fdf0b450de4b5747e6e0ed3e
MaPePeR <mapeper@example.com>
2013-09-22 14:51:36 +0200

    Further AuctionHouse-Implementation

--- AuctionHouse.cs
+++ AuctionHouse.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 namespace Auction.mod
 {
     public class AuctionHouse
     {
+        public enum SortMode {
+            CARD,SELLER,TIME,PRICE
+        }
         #region Singleton-Pattern
         private static readonly AuctionHouse instance = new AuctionHouse();
 
@@ -20,15 +24,43 @@ namespace Auction.mod
 
         public readonly AuctionFilter sellOfferFilter = new AuctionFilter();
         public readonly AuctionFilter buyOfferFilter = new AuctionFilter();
-        protected List<Auction> fullList = new List<Auction>();
+        //protected List<Auction> fullList = new List<Auction>();
         protected List<Auction> fullSellOfferList = new List<Auction>();
         protected List<Auction> fullBuyOfferList = new List<Auction>();
         protected List<Auction> sellOfferListFiltered = new List<Auction>();
         protected List<Auction> buyOfferListFiltered = new List<Auction>();
-        protected bool filtersChanged = false;
+        protected SortMode sortMode = SortMode.TIME;
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="Auction.mod.AuctionHouse"/> has unseen sell offers.
+        /// </summary>
+        /// <value><c>true</c> if new sell offers where added; otherwise, <c>false</c>.</value>
+        public bool newSellOffers { get; private set;}
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="Auction.mod.AuctionHouse"/> new unseen buy offers.
+        /// </summary>
+        /// <value><c>true</c> if new buy offers where added; otherwise, <c>false</c>.</value>
+        public bool newBuyOffers { get; private set;}
 
-        public void addAuction(Auction a) {
-            fullList.Add (a);
+        public ReadOnlyCollection<Auction> getBuyOffers() {
+            newBuyOffers = false;
+            if (buyOfferFilter.filtersChanged) {
+                buyOfferListFiltered = new List<Auction> (fullBuyOfferList);
+                buyOfferListFiltered.RemoveAll (buyOfferFilter.isFiltered);
+            }
+            return buyOfferListFiltered.AsReadOnly ();
+        }
+
+        public ReadOnlyCollection<Auction> getSellOffers() {
+            newSellOffers = false;
+            if (sellOfferFilter.filtersChanged) {
+                sellOfferListFiltered = new List<Auction> (fullSellOfferList);
+                sellOfferListFiltered.RemoveAll (sellOfferFilter.isFiltered);
+            }
+            return buyOfferListFiltered.AsReadOnly ();
+        }
+
+        private void addAuction(Auction a) {
+            //fullList.Add (a);
             if (a.offer == Auction.OfferType.BUY) {
                 fullBuyOfferList.Add (a);
                 if (!buyOfferFilter.isFiltered(a)) {
@@ -41,8 +73,16 @@ namespace Auction.mod
                 }
             }
         }
+        public void setSortMode(SortMode sortMode) {
+            this.sortMode = sortMode;
+        }
         public void addAuctions(List<Auction> list) {
             list.ForEach (addAuction);
+            fullBuyOfferList.Sort (Auction.getComparison(sortMode));
+            buyOfferListFiltered.Sort (Auction.getComparison(sortMode));
+            fullSellOfferList.Sort (Auction.getComparison(sortMode));
+            sellOfferListFiltered.Sort (Auction.getComparison(sortMode));
+
         }
     }
 }
